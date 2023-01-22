81fe7515b832ab4dd58464118dfba42282c4dd53
Jaan Kalbin <jaan.kalbin@tktk.ee>
2023-01-22 22:36:09 +0200

    add Week4 code

---
+++ ForLoopSum/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+int sum = 0;
+
+for (int i = 1; i < 13; i++) 
+{
+
+    Console.WriteLine($"i = {i}");
+    Console.WriteLine($"Current total: {sum}");
+    sum = sum + i; 
+}
+
+//Console.WriteLine($"Final total {sum}");
---
+++ Week4Loops/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int i = 0;
+
+for (i = 0; i < 5; i++)
+{
+    Console.WriteLine("We don't need no education.");
+}
+
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day!");
